1d5cccd3b780887c13f4411b47f7100c1fe4ba5e
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-04-01 21:21:27 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.05
-const image = 'https://pbs.twimg.com/media/FPHntTfakAEu8r4?format=jpg&name=medium'
-const Title = 'Chunky Chimps'
+const solAmount = 0.045
+const image = 'https://pbs.twimg.com/profile_images/1490829102093574150/kJpoL5R6_400x400.jpg'
+const Title = 'QUACKY DUCK'
 
 document.title = Title
 
