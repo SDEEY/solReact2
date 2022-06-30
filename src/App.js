09d30dbf557f451da5ec3fc267cd1b0dc18ca255
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-06-30 23:50:37 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 const solAmount = 0.035
-const image = 'https://howrare.is/drop_logos/5284_GSJH4m-D.jpg' 
-const Title = 'Degen Goblins'
-const supply = 1111
+const image = 'https://howrare.is/drop_logos/5350_07NbWk5Q.jpg' 
+const Title = 'Gun Race'
+const supply = 0
 
 document.title = Title
 
