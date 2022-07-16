b4256ae6813903ad43128687ad2f76c73ad01f44
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-07-16 23:57:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.1
-const image = 'https://howrare.is/drop_logos/5489_ehh8R6hd.jpg'
-const Title = 'Forest Industry'
-const supply = 777
+const solAmount = 1
+const image = 'https://howrare.is/drop_logos/5561_xNNnhbKm.jpg'
+const Title = 'ApeX Club NFT'
+const supply = 5555
 
 document.title = Title
 
