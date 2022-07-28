006d7c4c0873c0e9d1aa96783878a7c27b1adadd
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-07-29 00:52:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.35
-const image = 'https://howrare.is/drop_logos/5580_ff_-wr_Y.jpg'
-const Title = 'Beetle Punks'
-const supply = 3333
+const solAmount = 2
+const image = 'https://howrare.is/drop_logos/5562_jJAo9TA0.jpg'
+const Title = 'Flying Bee'
+const supply = 4444
 
 document.title = Title
 
