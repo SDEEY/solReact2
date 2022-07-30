1056570227740a2a1323dcd45aa190c1385d0485
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-07-31 00:54:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.069
-const image = 'https://howrare.is/drop_logos/5616_bMnNrn7P.jpg'
-const Title = 'Cyber Dragonz'
-const supply = 3333
+const solAmount = 1.3
+const image = 'https://howrare.is/drop_logos/4975_1XkxWtPt.jpg'
+const Title = 'SkyWings NFT'
+const supply = 7777
 
 document.title = Title
 
