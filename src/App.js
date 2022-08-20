bb164527471370a015f6f3a1ac27821c4b1299ba
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-08-20 20:52:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.3
-const image = 'https://pbs.twimg.com/profile_images/1545557940047863813/b5iyW9w4_400x400.jpg'
-const Title = 'GoopyWRLD'
-const supply = 7777
+const solAmount = 1
+const image = 'https://nftsolana.io/wp-content/uploads/2022/08/InfiniteDrive_-5.jpg'
+const Title = 'Infinite Drive'
+const supply = 3150
 
 document.title = Title
 
