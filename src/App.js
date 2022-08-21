8fccd4fd69746fcf83f82a1d84b8f6a39baf7335
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-08-21 22:56:02 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.3
-const image = 'https://pbs.twimg.com/profile_images/1556760830322016256/QMNx_uG9_400x400.jpg'
-const Title = 'CrazylabCrew_NFT'
-const supply = 6666
+const solAmount = 3
+const image = 'https://pbs.twimg.com/profile_images/1517498393727324160/LHxtYMEX_400x400.jpg'
+const Title = 'Eco-Friendly Turtles'
+const supply = 3333
 
 document.title = Title
 
