42414747158a1d239260a35d5b64201646f0bd87
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-08-23 20:52:38 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 const solAmount = 0.19
-const image = 'https://howrare.is/drop_logos/5368_S9e58w0q.jpg'
-const Title = 'SolPaintClub Book 3'
-const supply = 777
+const image = 'https://pbs.twimg.com/profile_images/1554486203700969473/FGvJu4ad_400x400.jpg'
+const Title = 'HOUNDZ'
+const supply = 2222
 
 document.title = Title
 
