57e7321306a55dcf12cc5ffc7b7babd9c2b99086
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-08-26 22:51:59 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.39
-const image = 'https://pbs.twimg.com/profile_images/1558932077206245376/CaDo9gw5_400x400.jpg'
-const Title = 'DEGEN FOMO GAMES'
-const supply = 999
+const solAmount = 1.2
+const image = 'https://pbs.twimg.com/profile_images/1532344215685971969/JtOERNgW_400x400.jpg'
+const Title = 'Soulofox'
+const supply = 3333
 
 document.title = Title
 
