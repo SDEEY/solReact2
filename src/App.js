42a437dfac6933918fc648387c0273d98997e6f3
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-08-31 22:54:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.1
-const image = 'https://howrare.is/drop_logos/5863_rfCVUJoS1.jpg'
-const Title = 'Fraction Zero Gen 2'
-const supply = 1500
+const solAmount = 2.5
+const image = 'https://pbs.twimg.com/profile_images/1532302432042364928/N3Frq4oC_400x400.jpg'
+const Title = '404 Arcane Valley'
+const supply = 444
 
 document.title = Title
 
