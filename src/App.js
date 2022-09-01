b0036d38b47878090986a3d5c06205c49acb8434
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-09-01 21:49:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 1
-const image = 'https://pbs.twimg.com/profile_images/1511399271861403649/Q2mjZxeG_400x400.jpg'
-const Title = 'Phantasia Sports'
-const supply = 1900
+const solAmount = 0.77
+const image = 'https://pbs.twimg.com/profile_images/1530904394543161347/yDkOuJ1__400x400.jpg'
+const Title = 'Blatant'
+const supply = 3333
 
 document.title = Title
 
